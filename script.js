7557d8a110af46cdcfe5d486bc4366bf7647992c
G <mail@grahamirvine.com>
2023-02-05 13:09:46 -0700

    basic javascript to check password and confirm password input are same, need to adjust style

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
-const passwordBox = document.getElementById('pwd')
-const confirmPasswordBox = document.getElementById('confirm-pwd')
+const passwordBoxes = document.querySelectorAll("input[type='password']")
 
-passwordBox.addEventListener('change' () )
+const pwdBox = document.getElementById('pwd')
+const confirmPwdBox = document.getElementById('confirm-pwd')
+
+
+/* passwordBox.addEventListener('change' () ) */
+
+passwordBoxes.forEach(box => {
+  box.addEventListener("blur", checkPwd, true)
+});
+
+passwordBoxes.forEach(box => {
+  box.addEventListener("focus", clearPwdBoxStyle, true)
+});
+
+function checkPwd() {
+  let pwd = pwdBox.value
+  let confirmPwd = confirmPwdBox.value
+
+  if (pwd == confirmPwd) {
+    pwdBox.style.backgroundColor = "green";
+    confirmPwdBox.style.backgroundColor = "green";
+  } else {
+    pwdBox.style.backgroundColor = "red";
+    confirmPwdBox.style.backgroundColor = "red";
+  }
+}
+
+function clearPwdBoxStyle() {
+  pwdBox.style.backgroundColor = "white";
+  confirmPwdBox.style.backgroundColor = "white";
+}
+
+
+
