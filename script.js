b77db79b291ed0938531779d5f0b54abe4fcdec7
G <mail@grahamirvine.com>
2023-02-05 14:17:37 -0700

    script to check password == confirm password works

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const passwordBoxes = document.querySelectorAll("input[type='password']")
 const pwdBox = document.getElementById('pwd')
 const confirmPwdBox = document.getElementById('confirm-pwd')
 const pwdListElement = document.querySelector(".pwd-li")
+const pwdErrorMsg = document.querySelector(".pwd-error-msg")
+
 
 
 
@@ -20,31 +22,20 @@ function checkPwd() {
   let pwd = pwdBox.value
   let confirmPwd = confirmPwdBox.value
 
-  if (pwd == confirmPwd) {
-    pwdBox.style.backgroundColor = "green";
-    confirmPwdBox.style.backgroundColor = "green";
-  } else {
+  if (pwd != confirmPwd) {
     pwdBox.style.backgroundColor = "red";
     confirmPwdBox.style.backgroundColor = "red";
-    pwdMismatchWarning;
+    pwdMismatchWarning()
   }
 }
 
 function clearPwdBoxStyle() {
   pwdBox.style.backgroundColor = "white";
   confirmPwdBox.style.backgroundColor = "white";
+  pwdErrorMsg.innerHTML = " "
 }
 
 function pwdMismatchWarning() {
-  pwdErrorMsg = document.createElement("p");
-  document.querySelector(".pwd-li").appendChild(pwdErrorMsg);
-
-  pwdErrorMsg.value = "*Password does not match";
+  pwdErrorMsg.innerHTML = "*Password does not match";
   pwdErrorMsg.style.color = "red";
-}
-
-
-function testCase() {
-  aDiv = document.createElement('div')
-  document.quer
 }
